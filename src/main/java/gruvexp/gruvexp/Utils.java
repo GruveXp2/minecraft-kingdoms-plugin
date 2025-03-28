97e1c474b927f07e3b28e91d03fefd794f34ac3e
David Alexander Danielsen <davidada@uia.no>
2025-03-28 20:38:09 +0100

    remove unused import

--- src/main/java/gruvexp/gruvexp/Utils.java
+++ src/main/java/gruvexp/gruvexp/Utils.java
@@ -5,7 +5,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.Door;
 import org.bukkit.entity.Player;
