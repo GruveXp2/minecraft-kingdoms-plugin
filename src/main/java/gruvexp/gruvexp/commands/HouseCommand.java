ca3997aec8d980d0a43fed9b16db22987971afb9
David Alexander Danielsen <gruveexp123@gmail.com>
2025-04-08 20:19:57 +0200

    reimplement /house command and some changes to House and Path

--- src/main/java/gruvexp/gruvexp/commands/HouseCommand.java
+++ src/main/java/gruvexp/gruvexp/commands/HouseCommand.java
@@ -1,19 +1,17 @@
 package gruvexp.gruvexp.commands;
 
-import gruvexp.gruvexp.core.Locality;
-import gruvexp.gruvexp.core.Citizen;
-import gruvexp.gruvexp.core.House;
-import gruvexp.gruvexp.core.KingdomsManager;
+import gruvexp.gruvexp.core.*;
 import gruvexp.gruvexp.path.Path;
 import gruvexp.gruvexp.rail.Coord;
-import org.bukkit.ChatColor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class HouseCommand implements CommandExecutor {
 
@@ -21,106 +19,103 @@ public class HouseCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
 
-        if (!(sender instanceof Player p)) {
-            return true;
-        }
-        String usage =  ChatColor.WHITE + "\nUsage: /house <kingdom> <district> <address> [add | get | list | set | remove]";
-
-        if (args.length < 4) {
-            p.sendMessage(ChatColor.RED + "Error: Too few arguments." + usage);
-            return true;
-        }
-
-        try {
-            String kingdomID = args[0];
-            String districtID = args[1];
-            String addressID = args[2];
-            Locality locality = KingdomsManager.getKingdom(kingdomID).getDistrict(districtID).getLocality(addressID);
-            String oper = args[3];
-            int houseNumber = 0;
-            if (!oper.equals("list")) {
-                if (args.length == 4) {
-                    throw new IllegalArgumentException(ChatColor.RED + "You must specify house number");
-                }
-                try {
-                    houseNumber = Integer.parseInt(args[4]);
-                } catch (NumberFormatException e) {
-                    throw new IllegalArgumentException(ChatColor.RED + "House numbers cant contain letters!");
-                }
-            }
-            switch (oper) {
-                case "add" -> {
-                    locality.addHouse(houseNumber);
-                    p.sendMessage("Successfully added new house with ID " + ChatColor.BLUE + houseNumber);
-                }
-                case "get" -> {
-                    House house = locality.getHouse(houseNumber);
-                    p.sendMessage(String.format("%sHouse %s%s%s: %s%s%s: %s%s %s%s%s:", ChatColor.UNDERLINE, ChatColor.GOLD, kingdomID, ChatColor.WHITE, ChatColor.GOLD, districtID, ChatColor.WHITE, ChatColor.GOLD, addressID, ChatColor.BLUE, houseNumber, ChatColor.WHITE));
-                    HashSet<Citizen> villagers = house.getCitizens();
-                    if (villagers != null) {
-                        StringBuilder citizens = new StringBuilder("Villagers: " + ChatColor.GREEN);
-                        for (Citizen citizen : villagers) {
-                            citizens.append(citizen.getName()).append(", ");
-                        }
-                        citizens.delete(citizens.length() - 2, citizens.length());
-                        p.sendMessage(citizens.toString());
-                    }
-                    Coord doorPos = house.getDoorPos();
-                    if (doorPos != null) {
-                        p.sendMessage(String.format("Door pos: %s%s", ChatColor.AQUA, doorPos));
-                    }
-                    Coord bedPos = house.getBedPos();
-                    if (bedPos != null) {
-                        p.sendMessage(String.format("Bed pos: %s%s", ChatColor.AQUA, bedPos));
-                    }
-                    Path exitPath = house.getExitPath();
-                    if (exitPath != null) {
-                        p.sendMessage(String.format("Exit path: %s%s", ChatColor.YELLOW, exitPath));
-                    }
-                }
-                case "list" -> p.sendMessage("WIP: print list of all housenumbers here");
-                case "set" -> {
-                    House house = locality.getHouse(houseNumber);
-                    if (args.length == 5) {
-                        throw new IllegalArgumentException(ChatColor.RED + "You must choose what property to set\nUsage:" + ChatColor.WHITE + "set [door_pos | bed_pos | exit_path]");
-                    }
-                    String property = args[5];
-                    switch (property) {
-                        case "door_pos" -> {
-                            if (args.length < 9) {
-                                throw new IllegalArgumentException(ChatColor.RED + "Not enough coords specified!");
-                            }
-                            Coord doorPos = new Coord(args[6], args[7], args[8]);
-                            house.setDoorPos(doorPos);
-                            p.sendMessage(String.format("Successfully set door pos of %s%s %s%s%s to %s%s %s %s", ChatColor.GOLD, addressID, ChatColor.BLUE, houseNumber, ChatColor.WHITE, ChatColor.AQUA, args[6], args[7], args[8]));
-                        }
-                        case "bed_pos" -> {
-                            if (args.length < 9) {
-                                throw new IllegalArgumentException(ChatColor.RED + "Not enough coords specified!");
-                            }
-                            Coord bedPos = new Coord(args[6], args[7], args[8]);
-                            house.setBedPos(bedPos);
-                            p.sendMessage(String.format("Successfully set bed pos of %s%s %s%s%s to %s%s %s %s", ChatColor.GOLD, addressID, ChatColor.BLUE, houseNumber, ChatColor.WHITE, ChatColor.AQUA, args[6], args[7], args[8]));
-                        }
-                        case "exit_path" -> {
-                            if (args.length == 6) {
-                                throw new IllegalArgumentException(ChatColor.RED + "You must specify the pathID");
-                            }
-                            String pathID = args[6];
-                            Path exitPath = locality.getPath(pathID);
-                            house.setExitPath(exitPath);
-                            p.sendMessage(String.format("Successfully set pathID of %s%s %s%s%s to %s%s", ChatColor.GOLD, addressID, ChatColor.BLUE, houseNumber, ChatColor.WHITE, ChatColor.YELLOW, pathID));
-                        }
-                    }
-                }
-                case "remove" -> throw new IllegalArgumentException(ChatColor.RED + "This functionality is not added yet!");
-            }
-        } catch (IllegalArgumentException e) {
-            p.sendMessage(e.getMessage());
-        }
-
-
-
+        if (!(sender instanceof Player p)) {return true;}
+        if (args.length == 0) {return false;}
+        Component result = processCommand(p, args, command);
+        p.sendMessage(result);
         return true;
+    }
+
+    private Component processCommand(Player p, String[] args, Command command) {
+        Locality locality = KingdomsManager.getSelectedLocality(p);
+        if (locality == null) return Component.text("You must specify the scope of this command (what locality you wanna work with)" +
+                "\nrun /kingdoms select <kingdom> <district> <locality>", NamedTextColor.RED);
+
+        int houseNumber;
+        try {
+            houseNumber = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            return Component.text("House number must be a number!", NamedTextColor.RED);
+        }
+        House house = locality.getHouse(houseNumber);
+        if (house == null) return Component.text(locality.id + " has no house with number " + houseNumber + "!", NamedTextColor.RED);
+        if (args.length == 1) return Component.text("You must specify an operation [info | set | add | remove]");
+
+        String oper = args[1];
+        switch (oper) {
+            case "info" -> {
+                Component doorLocation = house.getDoorPos() != null ? house.getDoorPos().name() : Component.text("none", NamedTextColor.YELLOW);
+                Component bedLocation = house.getBedPos() != null ? house.getBedPos().name() : Component.text("none", NamedTextColor.YELLOW);
+                Component residents = !house.getResidents().isEmpty() ?
+                        Component.text(String.join(", ", house.getResidents().stream().map(c -> c.name).collect(Collectors.toSet())), NamedTextColor.GREEN) :
+                        Component.text("none", NamedTextColor.YELLOW);
+
+                Component pathSection = house.getExitPath() != null ? Component.text("Connected to path section ").append(house.getExitPath().name()) :
+                        Component.text("Not connected to any path section", NamedTextColor.YELLOW);
+
+                return Component.text("House ").append(house.name()).append(Component.text(" has the following data:")).appendNewline()
+                        .append(Component.text("Door location: ")).append(doorLocation).appendNewline()
+                        .append(Component.text("Bed location: ")).append(bedLocation).appendNewline()
+                        .append(Component.text("Residents: ").append(residents)).appendNewline()
+                        .append(pathSection);
+            }
+            case "set" -> {
+                if (args.length == 2) return Component.text("You must specify what to set: [door_pos | bed_pos | exit_path] <value>", NamedTextColor.RED);
+                String property = args[2];
+                switch (property) {
+                    case "door_pos" -> {
+                        if (args.length < 6) return Component.text("You must specify the coordinates: set door_pos <x y z>", NamedTextColor.RED);
+                        return house.setDoorPos(new Coord(args[3], args[4], args[5]));
+                    }
+                    case "bed_pos" -> {
+                        if (args.length < 6) return Component.text("You must specify the coordinates: set bed_pos <x y z>", NamedTextColor.RED);
+                        return house.setBedPos(new Coord(args[3], args[4], args[5]));
+                    }
+                    case "exit_path" -> {
+                        if (args.length == 3) return Component.text("You must specify what path section to set: path <path section>", NamedTextColor.RED);
+                        String pathID = args[3];
+                        Path path = locality.getPath(pathID);
+                        if (path == null) return locality.address().append(Component.text(" has no path section named \"" + pathID + "\"!", NamedTextColor.RED));
+                        return house.setExitPath(path);
+                    }
+                    default -> {
+                        return Component.text("\"" + property + "\" is not a property of a house", NamedTextColor.RED);
+                    }
+                }
+            }
+            case "add" -> {
+                if (args.length == 2) return Component.text("You must specify what to add: (resident)", NamedTextColor.RED);
+                String feature = args[2];
+                if (feature.equals("resident")) {
+                    if (args.length == 3)
+                        return Component.text("You must specify which resident to add to this house: add resident <citizen>");
+                    String citizenName = args[3];
+                    Kingdom kingdom = locality.getDistrict().getKingdom();
+                    Citizen citizen = kingdom.getCitizen(citizenName);
+                    if (citizen == null) return Component.text("Found no citizen with name \"" + citizenName + "\" in kingdom ", NamedTextColor.RED).append(kingdom.name());
+
+                    return house.addResident(citizen);
+                }
+                return Component.text("\"" + feature + "\" is not a feature that can be added to a house", NamedTextColor.RED);
+            }
+            case "remove" -> {
+                if (args.length == 2) return Component.text("You must specify what to remove: (resident)", NamedTextColor.RED);
+                String feature = args[2];
+                if (feature.equals("resident")) {
+                    if (args.length == 3)
+                        return Component.text("You must specify which resident to remove from this house: remove resident <citizen>");
+                    String citizenName = args[3];
+                    Kingdom kingdom = locality.getDistrict().getKingdom();
+                    Citizen citizen = kingdom.getCitizen(citizenName);
+                    if (citizen == null) return Component.text("Found no citizen with name \"" + citizenName + "\" in kingdom ", NamedTextColor.RED).append(kingdom.name());
+
+                    return house.removeResident(citizen);
+                }
+                return Component.text("\"" + feature + "\" is not a feature that can be removed from a house", NamedTextColor.RED);
+            }
+            default -> {
+                return Component.text("Invalid operator! Must be [info | set | add | remove]", NamedTextColor.RED);
+            }
+        }
     }
 }
--- src/main/java/gruvexp/gruvexp/commands/HouseTabCompletion.java
+++ src/main/java/gruvexp/gruvexp/commands/HouseTabCompletion.java
@@ -1,10 +1,9 @@
 package gruvexp.gruvexp.commands;
 
 import gruvexp.gruvexp.Utils;
-import gruvexp.gruvexp.core.Locality;
-import gruvexp.gruvexp.core.District;
-import gruvexp.gruvexp.core.Kingdom;
-import gruvexp.gruvexp.core.KingdomsManager;
+import gruvexp.gruvexp.core.*;
+import gruvexp.gruvexp.rail.Coord;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
@@ -12,59 +11,48 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class HouseTabCompletion implements TabCompleter {
     @Nullable
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
 
-        Player p = (Player) sender;
+        if (!(sender instanceof Player p)) {return List.of();}
+        Locality locality = KingdomsManager.getSelectedLocality(p);
+        if (locality == null) return List.of(ChatColor.RED + "You must select a locality to work with!", "run /kingdoms select <kingdom> <district> <locality>");
 
-        if (args.length == 1) {
-            return new ArrayList<>(KingdomsManager.getKingdomIDs());
-        }
+        if (args.length == 1) return locality.getHouseIDs().stream().map(Object::toString).toList();
+        int houseNumber;
         try {
-            String kingdomID = args[0];
-            Kingdom kingdom = KingdomsManager.getKingdom(kingdomID);
-            if (args.length == 2) {
-                return new ArrayList<>(kingdom.getDistrictIDs());
-            }
-            String districtID = args[1];
-            District district = kingdom.getDistrict(districtID);
-            if (args.length == 3) {
-                return new ArrayList<>(district.getLocalityIDs());
-            }
-            String addressID = args[2];
-            Locality locality = district.getLocality(addressID);
-            if (args.length == 4) {
-                return List.of("add", "get", "list", "set", "remove");
-            }
-            String oper = args[3];
-            if (args.length == 5) {
-                switch (oper) {
-                    case "get":
-                    case "set":
-                    case "remove":
-                        return locality.getHouseIDs().stream().map(Object::toString).collect(Collectors.toList());
-                }
-            }
-            if (oper.equals("set")) {
-                if (args.length == 6) {
-                    return List.of("door_pos", "bed_pos", "exit_path");
-                }
-                String property = args[5];
-                if (property.equals("door_pos") || property.equals("bed_pos")) {
-                    return List.of(Utils.getTargetBlock(p, 10).toString());
-                } else if (property.equals("exit_path")) {
-                    return new ArrayList<>(locality.getPathIDs());
-                }
-            }
-        } catch (IllegalArgumentException e) {
-            return List.of(e.getMessage());
+            houseNumber = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            return List.of(ChatColor.RED + "House number must be a number!");
         }
-        return new ArrayList<>(0);
+        House house = locality.getHouse(houseNumber);
+        if (house == null) return List.of(ChatColor.RED + "Unknown house with house number " + args[0]);
+
+        if (args.length == 2) return List.of("info", "set", "add resident", "remove resident");
+        String oper = args[1];
+        switch (oper) {
+            case "set" -> {
+                if (args.length == 3) return List.of("door_pos", "bed_pos", "exit_path");
+                String property = args[2];
+                switch (property) {
+                    case "door_pos", "bed_pos" -> {
+                        Coord coord = Utils.getPlayerBlockCoords(p);
+                        return List.of(coord.toString());
+                    }
+                    case "exit_path" -> {
+                        return locality.getPathIDs().stream().toList();
+                    }
+                }
+            }
+            case "add", "remove" -> {
+                if (args.length == 3) return List.of("resident");
+                if (args.length == 4 && args[2].equals("resident")) return locality.getDistrict().getKingdom().getCitizenNames().stream().toList();
+            }
+        }
+        return List.of();
     }
 }
