4f2e276d7e2a903ccf0074b4c840b22a439b1b44
David Alexander Danielsen <gruveexp123@gmail.com>
2025-04-08 18:09:14 +0200

    add /kingdoms select functionality to set command scope

--- src/main/java/gruvexp/gruvexp/commands/KingdomsTabCompletion.java
+++ src/main/java/gruvexp/gruvexp/commands/KingdomsTabCompletion.java
@@ -1,13 +1,15 @@
 package gruvexp.gruvexp.commands;
 
+import gruvexp.gruvexp.core.District;
+import gruvexp.gruvexp.core.Kingdom;
 import gruvexp.gruvexp.core.KingdomsManager;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class KingdomsTabCompletion implements TabCompleter {
@@ -17,7 +19,20 @@ public class KingdomsTabCompletion implements TabCompleter {
         String oper = args[0];
         switch (oper) {
             case "add", "remove" -> {
-                return new ArrayList<>(KingdomsManager.getKingdomIDs());
+                return KingdomsManager.getKingdomIDs().stream().toList();
+            }
+            case "select" -> {
+                if (args.length == 2) return KingdomsManager.getKingdomIDs().stream().toList();
+
+                String kingdomID = args[1];
+                Kingdom kingdom = KingdomsManager.getKingdom(kingdomID);
+                if (kingdom == null) return List.of(ChatColor.RED + "Kingdom \"" + kingdomID + " doesnt exist!");
+                if (args.length == 3) return kingdom.getDistrictIDs().stream().toList();
+
+                String districtID = args[2];
+                District district = kingdom.getDistrict(districtID);
+                if (district == null) return List.of(ChatColor.RED + "District \"" + districtID + " doesnt exist!");
+                if (args.length == 4) return district.getLocalityIDs().stream().toList();
             }
         }
         return List.of();
