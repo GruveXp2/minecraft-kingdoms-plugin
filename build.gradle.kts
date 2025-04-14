33c247b4684965fe30f54176c6d8ab0ce0eef6c2
David Alexander Danielsen <davidada@uia.no>
2025-04-14 14:42:54 +0200

    add: all kingdoms objects are clickable as text

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ val buildNumber: Int = if (buildNumberFile.exists()) {
 }
 
 group = "gruvexp"
-version = "1.1.1-$buildNumber"
+version = "1.1.2-$buildNumber"
 description = "The plugin used on the kingdoms server"
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
