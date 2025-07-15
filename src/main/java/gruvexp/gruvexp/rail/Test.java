89cddd21c64afdd652f5661ac70303b33cc8bad5
gruve <gruveexp123@gmail.com>
2025-07-15 13:54:50 +0200

    remove protocollib from plugin not needed anymore as mojang fixed the minecart system

--- src/main/java/gruvexp/gruvexp/rail/Test.java
+++ src/main/java/gruvexp/gruvexp/rail/Test.java
@@ -1,9 +1,5 @@
 package gruvexp.gruvexp.rail;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.PacketContainer;
 import org.bukkit.Location;
 import org.bukkit.entity.Minecart;
 import org.bukkit.entity.Player;
@@ -47,50 +43,5 @@ public class Test {
     public static void rotate4(Player p) {
         Location loc = p.getLocation();
         p.setRotation(loc.getYaw()+20, loc.getPitch());
-    }
-
-    public static void packetMount1(Player p, Minecart cart) {
-        ProtocolManager manager = ProtocolLibrary.getProtocolManager();
-        PacketContainer mountPacket = manager.createPacket(PacketType.Play.Server.MOUNT); // a packet which makes the player mount the minecart
-        mountPacket.getIntegers().write(0, cart.getEntityId());
-        mountPacket.getIntegerArrays().write(0, new int[] {p.getEntityId()});
-        manager.broadcastServerPacket(mountPacket);
-    }
-
-    public static void packetMount2(Player p, Minecart cart) {
-        Location loc = p.getLocation();
-        float yaw = loc.getYaw();
-        float pitch = loc.getPitch();
-        ProtocolManager manager = ProtocolLibrary.getProtocolManager();
-        PacketContainer mountPacket = manager.createPacket(PacketType.Play.Server.MOUNT); // a packet which makes the player mount the minecart
-        mountPacket.getIntegers().write(0, cart.getEntityId());
-        mountPacket.getIntegerArrays().write(0, new int[] {p.getEntityId()});
-        manager.broadcastServerPacket(mountPacket);
-
-
-        byte xRot = (byte) ((pitch / 360.0) * 256);
-        byte yRot = (byte) ((yaw / 360.0) * 256);
-
-        PacketContainer rotationPacket = new PacketContainer(PacketType.Play.Server.ENTITY_LOOK);
-        rotationPacket.getIntegers().write(0, p.getEntityId());
-        rotationPacket.getBytes().write(0, yRot);
-        rotationPacket.getBytes().write(1, xRot); // getBytes funker ikke for index 1, pga de er i samme byte?
-        manager.broadcastServerPacket(rotationPacket);
-    }
-
-    public static void packetRotate(Player p) {
-        Location loc = p.getLocation();
-        float yaw = loc.getYaw() + 20;
-        float pitch = loc.getPitch();
-        ProtocolManager manager = ProtocolLibrary.getProtocolManager();
-
-        byte xRot = (byte) ((pitch / 360.0) * 256);
-        byte yRot = (byte) ((yaw / 360.0) * 256);
-
-        PacketContainer rotationPacket = new PacketContainer(PacketType.Play.Server.ENTITY_LOOK);
-        rotationPacket.getIntegers().write(0, p.getEntityId());
-        rotationPacket.getBytes().write(0, yRot);
-        rotationPacket.getBytes().write(1, xRot); // getBytes funker ikke for index 1, pga de er i samme byte?
-        manager.broadcastServerPacket(rotationPacket);
     }
 }
